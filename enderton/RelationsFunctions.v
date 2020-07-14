79918ca1edffe2e4ca59e138088c07fcd7dc80c3
Samwise Parkinson <stp59@cornell.edu>
2020-07-14 17:21:55 -0400

    prove fundamental property of ordered pairs

--- enderton/RelationsFunctions.v
+++ enderton/RelationsFunctions.v
@@ -29,8 +29,91 @@ Qed.
 
 Ltac ordpair x y := destruct (OrdPair_Exists x y).
 
+Lemma Single_And_Pair : forall x y z A, Singleton x A -> Pair y z A ->
+  x = y /\ x = z.
+Proof.
+  intros x y z A Hs Hp. split.
+  - assert (P : In y A). { apply Hp. left. trivial. }
+    apply Hs in P. rewrite P. trivial.
+  - assert (P : In z A). { apply Hp. right. trivial. }
+    apply Hs in P. rewrite P. trivial.
+Qed.
+
 Theorem Enderton3A : forall x y u v xy uv, OrdPair x y xy -> OrdPair u v uv ->
   xy = uv <-> x = u /\ y = v.
 Proof.
   intros x y u v xy uv Hxy Huv. split; intros H.
   - unfold OrdPair in Hxy, Huv.
+    singleton u. rename x0 into Su. rename H0 into HSu.
+    pair u v. rename x0 into Puv. rename H0 into HPuv.
+    singleton x. rename x0 into Sx. rename H0 into HSx.
+    pair x y. rename x0 into Pxy. rename H0 into HPxy.
+    assert (A : In Su xy). { rewrite <- H in Huv. apply Huv. left. assumption. }
+    assert (B : In Puv xy). { rewrite <- H in Huv. apply Huv. right. assumption. }
+    apply Hxy in A. apply Hxy in B. destruct A as [a | b].
+    + destruct B as [c | d].
+      * assert (P : x = u /\ x = v).
+        { apply (Single_And_Pair _ _ _ Puv); assumption. }
+        destruct P as [P Q].
+        assert (R : Singleton Su uv).
+        { intros p. split; intros I.
+          - apply Extensionality_Axiom. intros z. split.
+            + intros J. apply HSu. apply Huv in I.
+              destruct I as [I | I].
+              * apply I in J. assumption.
+              * apply I in J. destruct J as [J | J].
+                assumption. rewrite <- P. rewrite Q. assumption.
+            + intros J. apply HSu in J. apply Huv in I.
+              destruct I as [I | I].
+              * apply I. assumption.
+              * apply I. left. assumption.
+          - apply Huv. left. rewrite I. assumption. }
+        rewrite <- H in R. split; try assumption.
+        assert (S : In Pxy xy). { apply Hxy. right. assumption. }
+        apply R in S. assert (T : In y Pxy). apply HPxy. right. trivial.
+        rewrite S in T. apply HSu in T. rewrite T. rewrite <- P. assumption.
+      * assert (P : In x Su). { apply a. trivial. }
+        apply HSu in P. split; try assumption.
+        assert (Q : In y Puv). { apply d. right. trivial. }
+        apply HPuv in Q. destruct Q as [Q | Q]; try assumption.
+        assert (R : Singleton Sx xy).
+        { intros f. split; intros F.
+          - apply Extensionality_Axiom. intros g. split; intros G.
+            + apply HSx. apply Hxy in F. destruct F.
+              * apply H0 in G. assumption.
+              * apply H0 in G. destruct G; try assumption.
+                rewrite P. rewrite <- Q. assumption.
+            + apply HSx in G. apply Hxy in F. destruct F.
+              * apply H0. assumption.
+              * apply H0. left. assumption.
+          - apply Hxy. left. rewrite F. assumption. }
+        rewrite H in R.
+        assert (S : In Puv uv). { apply Huv. right. assumption. }
+        apply R in S. assert (T : In v Puv).
+        { apply HPuv. right. trivial. }
+        rewrite S in T. apply HSx in T.
+        rewrite T. rewrite Q. rewrite P. trivial.
+    + destruct B as [c | d].
+      * assert (P : In u Puv). { apply HPuv. left. trivial. }
+        apply c in P. split; try (rewrite P; trivial).
+        assert (Q : In v Puv). { apply HPuv. right. trivial. }
+        apply c in Q. rewrite Q. rewrite <- P.
+        assert (T : In y Su). { apply b. right. trivial. }
+        apply HSu in T. assumption.
+      * assert (P : In x Su). { apply b. left. trivial. }
+        apply HSu in P. split; try assumption.
+        assert (Q : In y Puv). { apply d. right. trivial. }
+        apply HPuv in Q. destruct Q as [Q | Q]; try assumption.
+        assert (R : u = v). { rewrite P in d. rewrite Q in d.
+          assert (T : In v Puv). { apply HPuv. right. trivial. }
+          apply d in T. destruct T as [T | T]; rewrite T; trivial. }
+        rewrite <- R. assumption.
+  - destruct H as [H I]. apply Extensionality_Axiom.
+    intros z. split; intros J.
+    + apply Huv. apply Hxy in J. destruct J as [J | J].
+      * left. rewrite <- H. assumption.
+      * right. rewrite <- H. rewrite <- I. assumption.
+    + apply Hxy. apply Huv in J. destruct J as [J | J].
+      * left. rewrite H. assumption.
+      * right. rewrite H, I. assumption.
+Qed.
